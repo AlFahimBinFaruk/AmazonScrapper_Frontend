7fcc25f4219a0c99b0602beae8863cacbc632384
al fahim bin faruk <fmd03700@gmail.com>
2022-06-20 20:50:27 +0600

    ready to host

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import DataList from "./pages/DataList";
-import Alert from "./components/Alert"
+import Alert from "./components/Alert";
 function App() {
   return (
     <div className="App container">
-      <Alert/>
+      <Alert />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/scrapted-data" element={<DataList />} />
+          <Route path="*" element={<h6>Route not found!!</h6>} />
         </Routes>
       </BrowserRouter>
     </div>
